f9724a188f315c3f5b57f473433574989bab981b
Mu2ch <mu2ch@mail.ru>
2015-09-06 13:30:57 +0300

    remove comment

--- typeclasses/npc.py
+++ typeclasses/npc.py
@@ -8,13 +8,7 @@ class CmdSetTest(CmdSet):
 
 class YourMom(DefaultObject):
     def at_object_creation(self):
-        # here we add the cmdset
         self.cmdset.add(CmdSetTest)
-        # we should lock the object so we can't
-        # call commands on it from "the outside",
-        # (only the npc itself should have access to
-        # this command). This is done with the "call"
-        # lock type.
         self.locks.add("call:false()")
     def at_object_receive(self, obj, source_location):
         self.execute_cmd("say Хороший сына растет, спасибо за %s, %s!" % (obj, source_location))
