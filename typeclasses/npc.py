b14207181f8d3631bd4bc4ffbade0d4eb1463e15
Mu2ch <mu2ch@mail.ru>
2015-09-06 13:24:20 +0300

    move Bayta class to typeclass

--- typeclasses/npc.py
+++ typeclasses/npc.py
@@ -18,3 +18,14 @@ class YourMom(DefaultObject):
         self.locks.add("call:false()")
     def at_object_receive(self, obj, source_location):
         self.execute_cmd("say Хороший сына растет, спасибо за %s, %s!" % (obj, source_location))
+
+class YourDad(DefaultObject):
+    def at_object_creation(self):
+        self.cmdset.add(CmdSetTest)
+        self.locks.add("call:false()")
+
+    def at_object_receive(self, obj, source_location):
+        if obj.key == "травы":
+          self.execute_cmd("say Ой, мне нужно в туалет")
+        else:
+          self.execute_cmd("say Путин - лучший президент!")
