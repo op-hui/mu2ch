3349268c50458e5c6491e301d7fd0a2091fe0cc9
copyright <copyright@namba.kg>
2015-09-09 01:38:15 +0600

    подкрашивание неписей

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -18,6 +18,42 @@ class Room(DefaultRoom):
     See examples/object.py for a list of
     properties and methods available on all Objects.
     """
+
+    def return_appearance(self, looker):
+        """
+        This formats a description. It is the hook a 'look' command
+        should call.
+
+        Args:
+            looker (Object): Object doing the looking.
+        """
+        if not looker:
+            return
+        # get and identify all objects
+        visible = (con for con in self.contents if con != looker and
+                                                    con.access(looker, "view"))
+        exits, users, things = [], [], []
+        for con in visible:
+            key = con.get_display_name(looker)
+            if con.destination:
+                exits.append(key)
+            elif con.has_player:
+                users.append("{c%s{n" % key)
+            elif con.db.npc:
+                things.append("{G%s{n" % key)
+            else:
+                things.append(key)
+        # get description, build string
+        string = "{c%s{n\n" % self.get_display_name(looker)
+        desc = self.db.desc
+        if desc:
+            string += "%s" % desc
+        if exits:
+            string += "\n{wExits:{n " + ", ".join(exits)
+        if users or things:
+            string += "\n{wYou see:{n " + ", ".join(users + things)
+        return string
+
     pass
 
 class SimpleRoom(DefaultRoom):
