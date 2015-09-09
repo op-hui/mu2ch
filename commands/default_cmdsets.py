4cf75d97852eb5072d89fcc557d6c90856fe9179
copyright <copyright@namba.kg>
2015-09-09 23:01:18 +0600

    Команды для комнат

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -15,6 +15,7 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 """
 
 from evennia import default_cmds
+from typeclasses.extended_room import CmdExtendedLook,CmdExtendedDesc,CmdGameTime
 from commands.help_ru import CmdHelp_ru,CmdUnconnectedHelp_ru
 from commands.command import CmdCreateNPC,CmdHomeRu,CmdLookRu,CmdInventoryRu,CmdGetRu,CmdDropRu,CmdGiveRu,CmdSayRu,CmdPoseRu,CmdDescRu,CmdTalk
 
@@ -46,6 +47,9 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(CmdPoseRu())
         self.add(CmdTalk())
         self.add(CmdHelp_ru())
+        self.add(CmdExtendedLook())
+        self.add(CmdExtendedDesc())
+        self.add(CmdGameTime())
 
 class PlayerCmdSet(default_cmds.PlayerCmdSet):
     """
