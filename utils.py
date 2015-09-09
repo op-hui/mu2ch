1b8b019ac7d4b669b81106780e5bd85158ff3bfb
Mu2ch <mu2ch@mail.ru>
2015-09-09 20:04:13 +0300

    Дебаг для создания туненля между локациями

--- utils.py
+++ utils.py
@@ -5,9 +5,10 @@ from evennia import create_object
 from evennia import settings
 
 def locationTunnel(location1, location1_exit_name, location2, location2_exit_name): 
+    print "Create tunnel between '%s' and '%s'" % (location1_exit_name, location2_exit_name) 
     create_object(settings.BASE_EXIT_TYPECLASS, location2_exit_name if (location2_exit_name) else location2.key, location = location1, destination = location2)
     create_object(settings.BASE_EXIT_TYPECLASS, location1_exit_name if (location1_exit_name) else location1.key, location = location2, destination = location1)
 
 def locationTunnelDefault(location1, location2): 
-    return locationTunnel(location1, location2.key, location2, location1.key) 
+    return locationTunnel(location1, None, location2, None) 
 
