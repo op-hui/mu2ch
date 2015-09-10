5eb5c6d680268d54004fe5a2d8b3c74c6db5d24e
copyright <copyright@namba.kg>
2015-09-10 12:36:57 +0600

    перевод сообщей о прибытии и убытии персонажа которым управляет игрок

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -36,4 +36,49 @@ class Character(DefaultCharacter):
             bugurts = [u"первый бугурт", u"второй бугурт"]
             self.execute_cmd("сказать " + random.choice(bugurts))
 
+    def announce_move_from(self, destination):
+        """
+        Called if the move is to be announced. This is
+        called while we are still standing in the old
+        location.
+
+        Args:
+            destination (Object): The place we are going to.
+
+        """
+        if not self.location:
+            return
+        name = self.name
+        loc_name = ""
+        loc_name = self.location.name
+        dest_name = destination.name
+        string = "%s уходит из %s, направляясь в %s."
+        self.location.msg_contents(string % (name, loc_name, dest_name), exclude=self)
+
+
+    def announce_move_to(self, source_location):
+        """
+        Called after the move if the move was not quiet. At this point
+        we are standing in the new location.
+
+        Args:
+            source_location (Object): The place we came from
+
+        """
+
+        name = self.name
+        if not source_location and self.location.has_player:
+            # This was created from nowhere and added to a player's
+            # inventory; it's probably the result of a create command.
+            string = "Теперь у тебя есть в распоряжении %s." % name
+            self.location.msg(string)
+            return
+
+        src_name = "nowhere"
+        loc_name = self.location.name
+        if source_location:
+            src_name = source_location.name
+        string = "%s пришел в %s из %s."
+        self.location.msg_contents(string % (name, loc_name, src_name), exclude=self)
+
 
