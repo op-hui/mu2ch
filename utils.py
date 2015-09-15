aff8bccba5f59c1850e2b49d7d6c1146c6272659
Mu2ch <mu2ch@mail.ru>
2015-09-15 21:14:12 +0300

    При заселении игрока, вход в квартиру доступен тольк оему и волшебникам

--- utils.py
+++ utils.py
@@ -10,8 +10,9 @@ def locationTunnel(location1, location1_exit_name, location2, location2_exit_nam
     exit1 = location2_exit_name if (location2_exit_name) else location2.key
     exit2 = location1_exit_name if (location1_exit_name) else location1.key
     print "Create tunnel between '%s' and '%s'" % ( exit1, exit2 ) 
-    create_object(settings.BASE_EXIT_TYPECLASS, exit1, location = location1, destination = location2)
-    create_object(settings.BASE_EXIT_TYPECLASS, exit2, location = location2, destination = location1)
+    exit1 = create_object(settings.BASE_EXIT_TYPECLASS, exit1, location = location1, destination = location2)
+    exit2 = create_object(settings.BASE_EXIT_TYPECLASS, exit2, location = location2, destination = location1)
+    return [ exit1, exit2 ]
 
 def locationTunnelDefault(location1, location2): 
     return locationTunnel(location1, None, location2, None) 
